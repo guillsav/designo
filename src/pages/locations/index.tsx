58bd15e103361fa3551693a0e1951028093446e3
guillsav <guillsav@gmail.com>
2021-06-22 11:50:04 -0400

    Location page in progress

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -7,7 +7,7 @@ export const Locations = () => (
         type="mosaic"
         title="Canada"
         addressHeader="Designo Central Office"
-        address="3886 Wellington Street Toronto, Ontario M9C 3J5"
+        address="3886 Wellington StreetToronto, Ontario M9C 3J5"
         phone="253-863-8967"
         email="contact@designo.co"
       />
